8e168d30f51a3ee10e7e9adc98a6657cc244415b
vaflichki <dpwflow@gmail.com>
2023-04-01 03:50:04 +0300

    interval app improvements

---
+++ Interval Timing vanilla JS/home.js
@@ -0,0 +1,56 @@
+// KAKVO MOJESH BE
+// metoda na programirane
+// 1- kakvo mojesh izpolzvai dokudeto sa ti poznaniqta
+// 2-ro kakvo znaesh, opitai se da sustavish plan - kakvo ti trqbwa, kak smqtash, che trqbwa da se postroi neshtoto, nai malko trqbwa da znaesh kakva ti e krainata cel kakvo trqbwa da se poluchi
+// 3- neshto kato reverse engineering - predstavqsh si gotoviq produkt i na baza tva pochvash da go gradish
+// 4 - zapochni ot nqkude, napravi neshto puk posle go dooformqi, podobrqvai i tn
+
+
+
+// increment function
+// tuka she trq i da se pazi 
+// trq imash nqkva promenliva , koqto da suhranqwa prednata nastroika, ne da e ravna na nula
+
+let secondsIncrement = document.getElementById("seconds");
+
+console.log(secondsIncrement);
+
+function incrementSeconds() {
+ secondsIncrement += 1;
+ console.log("the seconds now are: " + secondsIncrement)
+ secondsIncrement.innerText = secondsIncrement;
+
+}
+// she vzima HTML elementa i shte mu promenq stoinosta na teksta
+// ulovka tuka imash double
+// sushto trqbwa da ima predel da ne moje da nadvishawa incrementa poweche ot 99 sushto da ne moje da namalqwa poweche ot 00
+
+
+
+
+
+// decrement function
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
